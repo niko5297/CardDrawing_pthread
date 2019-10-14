7f174aade5dc68d2bc7c414b3ded949329bf325c
nikolajwassmann <nwassmann@hotmail.com>
2019-10-14 15:31:59 +0200

    Added arrays of cards

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <mach/boolean.h>
+#include <stdbool.h>
 
-char Hearts[13];
-char Spades[13];
-char Clubs[13];
-char Diamonds[13];
+char hearts[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+bool heartsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+char spades[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+bool spadesbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+char clubs[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+bool clubsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+char diamonds[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+bool diamondsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 
 
 int main() {
-    printf("Hello, World!\n");
+    printf("%c",hearts[12][0]);
     return 0;
 }
