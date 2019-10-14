d1c6e722264e0045556b6afb0a7be22346110744
nikolajwassmann <nwassmann@hotmail.com>
2019-10-14 16:01:30 +0200

    Random generator created for drawing cards using Pthread_mutex

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <mach/boolean.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 char hearts[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 bool heartsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
@@ -12,10 +13,17 @@ char clubs[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Que
 bool clubsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 char diamonds[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 bool diamondsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+    pthread_mutex_t pthread_draw, pthread_read;
 
 
-
-void *draw(void *arg){
+void *draw(){
+    int d;
+    pthread_mutex_lock(&pthread_draw);
+    // Current time as seed for random
+    srand(time(0));
+        d = rand()%13+1;
+        printf("%d ",d);
+    pthread_mutex_unlock(&pthread_draw);
 
 }
 
@@ -30,12 +38,11 @@ int main() {
     /**
      * Læs op på pthread_join
      */
-    pthread_t pthread_draw, pthread_read;
-    pthread_create(&pthread_draw,NULL,draw, (void*)&pthread_draw);
-    pthread_create(&pthread_read,NULL,read, (void*)&pthread_read);
+    pthread_mutex_init(&pthread_draw,NULL);
+    pthread_mutex_init(&pthread_read,NULL);
 
 
+    draw();
 
-
-    printf("%c",hearts[12][0]);
+    //printf("%c",hearts[13][0]);
 }
