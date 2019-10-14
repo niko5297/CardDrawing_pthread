76d3d587887555a371f4e915d9636b5fd063ed2c
nikolajwassmann <nwassmann@hotmail.com>
2019-10-14 15:44:14 +0200

    Created pthreads

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <mach/boolean.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 char hearts[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 bool heartsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
@@ -13,7 +14,28 @@ char diamonds[14][13] = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","
 bool diamondsbool[14] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 
 
+
+void *draw(void *arg){
+
+}
+
+void *read(void *arg){
+
+}
+
+
+
+
 int main() {
+    /**
+     * Læs op på pthread_join
+     */
+    pthread_t pthread_draw, pthread_read;
+    pthread_create(&pthread_draw,NULL,draw, (void*)&pthread_draw);
+    pthread_create(&pthread_read,NULL,read, (void*)&pthread_read);
+
+
+
+
     printf("%c",hearts[12][0]);
-    return 0;
 }
